d05b3e8d8de768b9590d98c22fc861bb23dd1801
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-03-29 16:26:49 -0700

    Update is_looping.c

--- level_1/is_looping/is_looping.c
+++ level_1/is_looping/is_looping.c
@@ -69,130 +69,57 @@ Example 4:
 In this case, it should return 1 (the loop begins at node 14).
 */
 
-struct s_node
-{
-    int value;
-    struct s_node *next;
-};
+typedef struct s_node {
+	int value;
+	struct s_node *next;
+} Node;
 
-int is_looping(struct s_node *node)
-{
-    if (!node)
-        return (0);
+int is_looping(Node* node) {
+	
+	Node* n1 = node;
+	Node* n2 = node;
 
-    struct s_node *slow = node;
-    struct s_node *fast = node->next;
+	while (n1 && n2) {
 
-    while (slow && fast && fast->next)
-    {
-        if (slow == fast)
-            return (1);
-        fast = fast->next->next;
-        slow = slow->next;
-    }
-    return (0);
+		n1 = n1->next;
+		n2 = n2->next;
+		if (n2)
+			n2 = n2->next;
+		if (n1 && n2 && n1 == n2)
+			return 1;
+	}
+	return 0;
 }
 
-// Comment below part
+/************
+  Test Main
+************/
 
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-
-#define BLACK "\x1b[30m"
-#define RED "\x1b[31m"
-#define GREEN "\x1b[32m"
-#define YELLOW "\x1b[33m"
-#define BLUE "\x1b[34m"
-#define MAGENTA "\x1b[35m"
-#define CYAN "\x1b[36m"
-#define WHITE "\x1b[37m"
-#define BBLACK "\x1b[40m"
-#define BRED "\x1b[41m"
-#define BGREEN "\x1b[42m"
-#define BYELLOW "\x1b[43m"
-#define BBLUE "\x1b[44m"
-#define BMAGENTA "\x1b[45m"
-#define BCYAN "\x1b[46m"
-#define BWHITE "\x1b[47m"
-#define RESET "\x1b[0m"
-#define CLEAR "\033c"
-#define BOLD "\033[1m"
-
-// struct s_node
-// {
-//     int value;
-//     struct s_node *next;
-// };
-
-// int is_looping(struct s_node *node);
-
-struct s_node *create_node(int value)
-{
-    struct s_node *new = (struct s_node *)malloc(sizeof(struct s_node));
-    new->value = value;
-    new->next = NULL;
-    return (new);
+#include <stdio.h>
+Node* b(int v) {
+	Node* new = malloc(sizeof(Node));
+	new->value = v;
+	new->next = 0;
+	return new;
 }
+int main() {
 
-int main()
-{
-    struct s_node *root;
-    struct s_node *tmp;
-    struct s_node *loop;
-    int il;
+	Node* a = b(1);
 
-    root = create_node(1);
-    root->next = create_node(2);
-    tmp = root->next;
-    tmp->next = create_node(3);
-    tmp = tmp->next;
-    loop = tmp;
-    tmp->next = create_node(4);
-    tmp = tmp->next;
-    tmp->next = create_node(5);
-    tmp = tmp->next;
-    tmp->next = create_node(6);
-    tmp = tmp->next;
-    tmp->next = loop;
+	a->next = b(2);
+	a->next->next = b(3);
+	a->next->next->next = b(4);
+	a->next->next->next->next = a;
 
-    il = is_looping(root);
-    printf(BOLD "EXAMPLE 1:\n" RESET);
-    printf(YELLOW "Expected: " BOLD "1\n" RESET);
-    printf("%sOutput: " BOLD "%d\n" RESET, il ? GREEN : RED, il);
+	printf("%d\n", is_looping(a));
 
-    root->value = 12;
-    loop = root->next;
-    loop->value = 150;
-    tmp = loop->next;
-    tmp->value = 30;
-    tmp = tmp->next;
-    tmp->value = 50;
-    tmp = tmp->next;
-    tmp->value = 345;
-    tmp = tmp->next;
-    tmp->value = 120;
-    tmp->next = create_node(200);
-    tmp->next->next = loop;
+	Node* c = b(1);
+	c->next = b(2);
+	c->next->next = b(3);
+	c->next->next->next = b(4);
 
-    il = is_looping(root);
-    printf(BOLD "EXAMPLE 2:\n" RESET);
-    printf(YELLOW "Expected: " BOLD "1\n" RESET);
-    printf("%sOutput: " BOLD "%d\n" RESET, il ? GREEN : RED, il);
-
-    tmp->next = NULL;
-
-    il = is_looping(root);
-    printf(BOLD "EXAMPLE 3:\n" RESET);
-    printf(YELLOW "Expected: " BOLD "0\n" RESET);
-    printf("%sOutput: " BOLD "%d\n" RESET, il ? RED : GREEN, il);
-
-    loop->next->value = 14;
-    loop->next->next = loop->next;
-
-    il = is_looping(root);
-    printf(BOLD "EXAMPLE 4:\n" RESET);
-    printf(YELLOW "Expected: " BOLD "1\n" RESET);
-    printf("%sOutput: " BOLD "%d\n" RESET, il ? GREEN : RED, il);
+	printf("%d\n", is_looping(c));
+	
+	return 0;
 }
