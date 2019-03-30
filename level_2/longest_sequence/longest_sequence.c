5340679b5546262044be6890a16d3ba92d0c0cc5
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-03-29 17:33:57 -0700

    Update longest_sequence.c

--- level_2/longest_sequence/longest_sequence.c
+++ level_2/longest_sequence/longest_sequence.c
@@ -48,3 +48,37 @@ Example 4:
 NULL
 In this case, as the root node is null, your function should return 0.
 */
+
+typedef struct		s_node
+{
+	int value;
+	struct s_node	*left;
+	struct s_node	*right;
+}					t_node;
+
+int	ft_max(int a, int b)
+{
+	return (a > b ? a : b);
+}
+
+void	penetrate_deep(t_node *last, t_node *this, int seq, int *n)
+{
+	if (this)
+	{
+		seq = (this->value - last->value == 1) ? seq + 1 : 1;
+		*n = ft_max(*n, seq);
+		penetrate_deep(this, this->left, seq, n);
+		penetrate_deep(this, this->right, seq, n);
+	}
+}
+
+int	longest_sequence(struct s_node *node)
+{
+	int	n = 1;
+
+	if (!node)
+		return (0);
+	penetrate_deep(node, node->left, 1, &n);
+	penetrate_deep(node, node->right, 1, &n);
+	return (n);
+}
