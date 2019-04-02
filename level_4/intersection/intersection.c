0824aedb2a61a6010130771e090887420af6652d
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-04-01 17:51:18 -0700

    Update intersection.c

--- level_4/intersection/intersection.c
+++ level_4/intersection/intersection.c
@@ -20,3 +20,25 @@ The function must be declared as follows:
 If the two linked lists are not intersecting, the function returns NULL.
 The function does not modify the two linked list.
 */
+
+struct s_node {
+	void *content;
+	struct s_node *next;
+};
+
+#include <stdlib.h>
+void *intersection(struct s_node *lst1, struct s_node *lst2)
+{
+	while (lst1)
+	{
+		struct s_node *temp = lst2;
+		while (temp)
+		{
+			if (lst1 == temp)
+				return lst1;
+			temp = temp->next;
+		}
+		lst1 = lst1->next;
+	}
+	return NULL;
+}
